ff9937c55795fb845e9edaea84ac5e2918d73555
Sarath <sarath.u1398@gmail.com>
2026-01-04 02:11:37 +0530

    Added Selection Sort program

---
+++ Sorting Algorithms/Program.cs
@@ -0,0 +1,12 @@
+﻿using Code_Implementations;
+
+namespace Sorting_Algorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SelectionSort.SelectionSortFunction([12, 2, 65, 34, 67, 54, 23, 86], 8);
+        }
+    }
+}
---
+++ Sorting Algorithms/SelectionSort.cs
@@ -0,0 +1,23 @@
+﻿namespace Code_Implementations
+{
+    public class SelectionSort
+    {
+        public static void SelectionSortFunction(int[] arr, int n)
+        {
+            // The last element is always the min/max value
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                (arr[i], arr[minIndex]) = (arr[minIndex], arr[i]);
+            }
+            Console.WriteLine(String.Join(',', arr));
+        }
+    }
+}
