2c1b169b30b84f7a39cdf3f6c3ef05fb8ecf742e
Sarath <sarath.u1398@gmail.com>
2026-01-04 16:48:50 +0530

    Added QuickSort algorithm

--- Sorting Algorithms/Program.cs
+++ Sorting Algorithms/Program.cs
@@ -7,6 +7,9 @@
             SelectionSort.SelectionSortFunction([12, 2, 65, 34, 67, 54, 23, 86], 8);
             BubbleSort.BubbleSortFunction([12, 2, 65, 34, 67, 54, 23, 86], 8);
             InsertionSort.InsertionSortFunction([12, 2, 65, 34, 67, 54, 23, 86], 8);
+            int[] arr = [12, 2, 65, 34, 67, 54, 23, 86];
+            QuickSort.QuickSortFunction(arr, 0, arr.Length - 1);
+            Console.WriteLine(string.Join(',',arr));
         }
     }
 }
---
+++ Sorting Algorithms/QuickSort.cs
@@ -0,0 +1,45 @@
+﻿namespace Sorting_Algorithms
+{
+    internal class QuickSort
+    {
+        public static int GetPivot(int[] arr, int low, int high)
+        {
+            int randomValue = new Random().Next(low, high + 1);
+            (arr[low], arr[randomValue]) = (arr[randomValue], arr[low]);
+            return GetPartitionIndex(arr,low,high);
+        }
+        public static int GetPartitionIndex(int[] arr, int low, int high)
+        {
+            int pivot = arr[low];
+            int i = low;
+            int j = high;
+
+            while (i < j)
+            {
+                while (pivot >= arr[i] && i < high)
+                {
+                    i++;
+                }
+                while (arr[j] > pivot && j > low)
+                {
+                    j--;
+                }
+                if (i < j)
+                {
+                    (arr[i], arr[j]) = (arr[j], arr[i]);
+                }
+            }
+            (arr[low], arr[j]) = (arr[j], arr[low]);
+            return j;
+        }
+        public static void QuickSortFunction(int[] arr, int low, int high)
+        {
+            if (low < high)
+            {
+                int pIndex = GetPivot(arr, low, high);
+                QuickSortFunction(arr, low, pIndex - 1);
+                QuickSortFunction(arr, pIndex + 1, high);
+            }
+        }
+    }
+}
