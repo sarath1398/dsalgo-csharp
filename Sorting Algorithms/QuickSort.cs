6e047ca6da6366bc107813574c90eeb2d9cf4f8b
Sarath <sarath.u1398@gmail.com>
2026-01-04 16:55:32 +0530

    Added comments to quick sort

--- Sorting Algorithms/QuickSort.cs
+++ Sorting Algorithms/QuickSort.cs
@@ -2,6 +2,8 @@
 {
     internal class QuickSort
     {
+        // Create a random pivot index to reduce choosing the highest
+        // element or the lowest element in an sorted array all the time
         public static int GetPivot(int[] arr, int low, int high)
         {
             int randomValue = new Random().Next(low, high + 1);
@@ -16,19 +18,28 @@
 
             while (i < j)
             {
+                // Skips all the element that are lesser than the pivot element
+                // from the left
                 while (pivot >= arr[i] && i < high)
                 {
                     i++;
                 }
+                // Skips all the element that are greater than the pivot element
+                // to the right
                 while (arr[j] > pivot && j > low)
                 {
                     j--;
                 }
+                // Swap the greater than pivot element from the left
+                // and the lesser than pivot element from the right
+                // if the j'th index does not cross the i'th index
                 if (i < j)
                 {
                     (arr[i], arr[j]) = (arr[j], arr[i]);
                 }
             }
+            // Swap the pivot with the lowest element
+            // to place the pivot in the correct position
             (arr[low], arr[j]) = (arr[j], arr[low]);
             return j;
         }
@@ -37,7 +48,9 @@
             if (low < high)
             {
                 int pIndex = GetPivot(arr, low, high);
+                // Repeat the process for arr[0..partitionIndex - 1] subarray
                 QuickSortFunction(arr, low, pIndex - 1);
+                // Repeat the process for arr[partitionIndex + 1..arr.Length - 1] subarray
                 QuickSortFunction(arr, pIndex + 1, high);
             }
         }
