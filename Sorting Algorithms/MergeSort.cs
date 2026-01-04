89d50acabfe192f3300900f0d7e09b2976caae90
Sarath <sarath.u1398@gmail.com>
2026-01-05 01:35:27 +0530

    Added comments

--- Sorting Algorithms/MergeSort.cs
+++ Sorting Algorithms/MergeSort.cs
@@ -4,9 +4,9 @@
     {
         public static void Merge(int[] arr, int low, int mid, int high)
         {
-            int i = low;
-            int j = mid+1;
-            int k = 0;
+            int i = low; // Points to the subarray arr[low...mid]
+            int j = mid+1; // Points to the subarray arr[mid+1...high]
+            int k = 0; // Points to the temp array
             int[] temp = new int[high - low + 1];
             while (i <= mid && j <= high)
             {
@@ -19,6 +19,8 @@
                     temp[k++] = arr[j++];
                 }
             }
+
+            // Add the remaining elements to the temp array if exists
             while (i <= mid)
             {
                 temp[k++] = arr[i++];
@@ -27,6 +29,7 @@
             {
                 temp[k++] = arr[j++];
             }
+            // Replace arr[low...high] with the values of temp
             for (int t = 0; t < temp.Length; t++)
             {
                 arr[low + t] = temp[t];
@@ -34,6 +37,8 @@
         }
         public static void MergeSortFunction(int[] arr, int low, int high)
         {
+            // Base case : when recursion happens till only one element exists between arr[low..high]
+            // i.e, Check whether the low and high are the same
             if (low >= high)
                 return;
             int mid = (low + high) / 2;
