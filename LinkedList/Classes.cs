23bcf5020c054758773f1c90c762e92392359497
Sarath <sarath.u1398@gmail.com>
2026-02-08 15:42:00 +0530

    Added LinkedList solution with singly linked list implementation

---
+++ LinkedList/Classes.cs
@@ -0,0 +1,110 @@
+﻿namespace LinkedList
+{
+    internal class Classes
+    {
+        public class ListNode(int val = 0, ListNode? next = null)
+        {
+            public int val = val;
+            public ListNode? next = next;
+        }
+
+        // NOTE : the hasTail parameter exists just for learning purposes only
+        // It is assumed that the state of hasTail will either be true or false
+        // throughout runtime and not a mix of both
+        public class SinglyLinkedList
+        {
+            public ListNode? head = null;
+            public ListNode? tail = null;
+
+            public void InsertAtBeginning(int value,bool hasTail = false)
+            {
+                ListNode? newNode = new(value, null);
+
+                if (head == null && hasTail)
+                {
+                    head = newNode;
+                    tail = newNode;
+                }
+                else if (head == null && !hasTail)
+                {
+                    head = newNode;
+                }
+                else
+                {
+                    newNode.next = head;
+                    head = newNode;
+                }
+            }
+
+            public void InsertAtEnd(int value, bool hasTail = false)
+            {
+                ListNode? newNode = new(value, null);
+
+                if (head == null && hasTail)
+                {
+                    head = newNode;
+                    tail = newNode;
+                }
+                else if (head == null && !hasTail)
+                {
+                    head = newNode;
+                }
+                else
+                {
+                    if (hasTail)
+                    {
+                        // O(1) operation
+                        tail?.next = newNode;
+                        tail = newNode;
+                    }
+                    else
+                    {
+                        // Takes O(n) without tail
+                        ListNode? ptr = head;
+                        while (ptr?.next != null)
+                        {
+                            ptr = ptr.next;
+                        }
+                        ptr?.next = newNode;
+                    }
+                }              
+            }
+
+            public void InsertAtPos(int value, int pos, bool hasTail = false)
+            {
+                ListNode newNode = new(value);
+
+                // using a dummy node for handling 1-indexing complexities
+                ListNode? dummy = new(0, head);
+                ListNode? prev = dummy;
+
+                for (int i = 0; i < pos - 1 && prev != null; i++)
+                {
+                    prev = prev?.next;
+                }
+
+                if (prev == null) return;
+
+                newNode.next = prev.next;
+                prev.next = newNode;
+
+                head = dummy.next;
+                if (newNode.next == null && hasTail)
+                {
+                    tail = newNode;
+                }
+            }
+
+            public void PrintList()
+            {
+                ListNode? cur = head;
+                while (cur != null)
+                {
+                    Console.Write(cur.val + " -> ");
+                    cur = cur.next;
+                }
+                Console.Write("NULL \n");
+            }
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,42 @@
+﻿using static LinkedList.Classes;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Singly Linked List without a tail pointer");
+            SinglyLinkedList list = new();
+            list.InsertAtBeginning(1);
+            list.InsertAtBeginning(2);
+            list.InsertAtBeginning(3);
+            list.InsertAtBeginning(4);
+            list.PrintList();
+            list.head = null;
+            list.InsertAtEnd(1);
+            list.InsertAtEnd(2);
+            list.InsertAtEnd(3);
+            list.InsertAtEnd(4);
+            list.InsertAtPos(5, 1);
+            list.PrintList();
+
+            Console.WriteLine("Singly Linked List with a tail pointer");
+
+            list.head = null;
+            list.InsertAtBeginning(1,true);
+            list.InsertAtBeginning(2,true);
+            list.InsertAtBeginning(3,true);
+            list.InsertAtBeginning(4,true);
+            list.PrintList();
+            list.head = null;
+            list.InsertAtEnd(1,true);
+            list.InsertAtEnd(2,true);
+            list.InsertAtEnd(3,true);
+            list.InsertAtEnd(4,true);
+            list.InsertAtPos(5, 5, true);
+            list.InsertAtEnd(6, true);
+            list.PrintList();
+        }
+    }
+}
